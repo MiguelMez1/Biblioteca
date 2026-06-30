4a287fce58b51bec792035f6d4c9752b976d3ff8
raulinojulia03 <raulino.julia03@gmail.com>
2026-06-30 20:13:51 -0300

    ajustes busca
    Foram feitos ajustes e continuamos o andamento nas outras opções de busca, foi adicionada as funções para título, autor, categoria, ano. Foi adicionado switch case para navegar pelos meios de busca.

--- gestao_busca.c
+++ gestao_busca.c
@@ -1,59 +1,62 @@
-#include <stdio.h>
-#include <windows.h>
-#include <string.h>
 
-#define QTD_LIVROS 3
-#define TAM_NOME 50
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+
+#define QTD_LIVROS 2
+#define TAM_TEXTO 100
 
 typedef struct {
-    int id;
-    char livro[TAM_NOME];
     int codigo;
+    char titulo[TAM_TEXTO];
+    char autor[TAM_TEXTO];
+    char categoria[TAM_TEXTO];
+    int ano;
 } Livro;
 
-// Protótipos
 void cadastrarLivros(Livro livros[]);
-void menuProduto();
-void buscarCodigo(Livro livros[], int total);
+void menu();
+void limparBuffer();
+void buscarPorCodigo(Livro livros[], int total);
+void buscarPorTitulo(Livro livros[], int total);
+void buscarPorAutor(Livro livros[], int total);
+void buscarPorCategoria(Livro livros[], int total);
+void buscarPorAno(Livro livros[], int total);
+void exibirLivro(Livro livro);
 
-int main()
-{
+int main() {
     SetConsoleOutputCP(CP_UTF8);
 
     Livro livros[QTD_LIVROS];
     int opcao;
-    int total = QTD_LIVROS;
 
-    printf("=== CADASTRO DE LIVROS ===\n");
+    printf("===== CADASTRO DE LIVROS =====\n");
     cadastrarLivros(livros);
 
-    do
-    {
-        menuProduto();
+    do {
+        menu();
         scanf("%d", &opcao);
+        limparBuffer();
 
-        while(getchar() != '\n');
-
-        switch(opcao)
-        {
+        switch (opcao) {
             case 1:
-                buscarCodigo(livros, total);
+                buscarPorCodigo(livros, QTD_LIVROS);
                 break;
 
             case 2:
-                
+                buscarPorTitulo(livros, QTD_LIVROS);
                 break;
 
             case 3:
-                
+                buscarPorAutor(livros, QTD_LIVROS);
                 break;
 
             case 4:
-                
+                buscarPorCategoria(livros, QTD_LIVROS);
                 break;
 
             case 5:
-                
+                buscarPorAno(livros, QTD_LIVROS);
                 break;
 
             case 0:
@@ -64,68 +67,155 @@ int main()
                 printf("\nOpção inválida!\n");
         }
 
-    }while(opcao != 0);
+    } while (opcao != 0);
 
     return 0;
 }
 
-void menuProduto()
-{
-    printf("\n===== MENU =====\n");
-    printf("1 - Buscar por código\n");
-    printf("2 - Buscar por título\n");
-    printf("3 - Buscar por autor\n");
-    printf("4 - Buscar por categoria\n");
-    printf("5 - Buscar por ano\n");
+void menu() {
+    printf("\n==== MENU DE BUSCA ====\n");
+    printf("1 - Código\n");
+    printf("2 - Título\n");
+    printf("3 - Autor\n");
+    printf("4 - Categoria\n");
+    printf("5 - Ano\n");
     printf("0 - Sair\n");
     printf("Escolha uma opção: ");
 }
 
-void cadastrarLivros(Livro livros[])
-{
-    for(int i = 0; i < QTD_LIVROS; i++)
-    {
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void cadastrarLivros(Livro livros[]) {
+    for (int i = 0; i < QTD_LIVROS; i++) {
+
         printf("\n--- Livro %d ---\n", i + 1);
-
-        livros[i].id = i + 1;
-
-        printf("Título: ");
-        fgets(livros[i].livro, TAM_NOME, stdin);
-        livros[i].livro[strcspn(livros[i].livro, "\n")] = '\0';
 
         printf("Código: ");
         scanf("%d", &livros[i].codigo);
+        limparBuffer();
 
-        while(getchar() != '\n');
+        printf("Título: ");
+        fgets(livros[i].titulo, TAM_TEXTO, stdin);
+        livros[i].titulo[strcspn(livros[i].titulo, "\n")] = '\0';
+
+        printf("Autor: ");
+        fgets(livros[i].autor, TAM_TEXTO, stdin);
+        livros[i].autor[strcspn(livros[i].autor, "\n")] = '\0';
+
+        printf("Categoria: ");
+        fgets(livros[i].categoria, TAM_TEXTO, stdin);
+        livros[i].categoria[strcspn(livros[i].categoria, "\n")] = '\0';
+
+        printf("Ano: ");
+        scanf("%d", &livros[i].ano);
+        limparBuffer();
     }
 }
 
-void buscarCodigo(Livro livros[], int total)
-{
-    int codBusca;
+void exibirLivro(Livro livro) {
+    printf("\n==== LIVRO ENCONTRADO ====\n");
+    printf("Código: %d\n", livro.codigo);
+    printf("Título: %s\n", livro.titulo);
+    printf("Autor: %s\n", livro.autor);
+    printf("Categoria: %s\n", livro.categoria);
+    printf("Ano: %d\n", livro.ano);
+}
+
+void buscarPorCodigo(Livro livros[], int total) {
+    int codigo;
     int encontrado = 0;
 
-    printf("\nDigite o código do livro: ");
-    scanf("%d", &codBusca);
+    printf("\nDigite o código: ");
+    scanf("%d", &codigo);
+    limparBuffer();
 
-    while(getchar() != '\n');
-
-    for(int i = 0; i < total; i++)
-    {
-        if(livros[i].codigo == codBusca)
-        {
-            printf("\n===== LIVRO ENCONTRADO =====\n");
-            printf("ID: %d\n", livros[i].id);
-            printf("Título: %s\n", livros[i].livro);
-            printf("Código: %d\n", livros[i].codigo);
-
+    for (int i = 0; i < total; i++) {
+        if (livros[i].codigo == codigo) {
+            exibirLivro(livros[i]);
             encontrado = 1;
             break;
         }
     }
 
-    if(!encontrado)
-    {
+    if (!encontrado)
         printf("\nLivro não encontrado!\n");
+}
+
+void buscarPorTitulo(Livro livros[], int total) {
+    char titulo[TAM_TEXTO];
+    int encontrado = 0;
+
+    printf("\nDigite o título: ");
+    fgets(titulo, TAM_TEXTO, stdin);
+    titulo[strcspn(titulo, "\n")] = '\0';
+
+    for (int i = 0; i < total; i++) {
+        if (strstr(livros[i].titulo, titulo) != NULL) {
+            exibirLivro(livros[i]);
+            encontrado = 1;
+        }
     }
+
+    if (!encontrado)
+        printf("\nNenhum livro encontrado.\n");
+}
+
+void buscarPorAutor(Livro livros[], int total) {
+    char autor[TAM_TEXTO];
+    int encontrado = 0;
+
+    printf("\nDigite o autor: ");
+    fgets(autor, TAM_TEXTO, stdin);
+    autor[strcspn(autor, "\n")] = '\0';
+
+    for (int i = 0; i < total; i++) {
+        if (strstr(livros[i].autor, autor) != NULL) {
+            exibirLivro(livros[i]);
+            encontrado = 1;
+        }
+    }
+
+    if (!encontrado)
+        printf("\nNenhum livro encontrado.\n");
+}
+
+void buscarPorCategoria(Livro livros[], int total) {
+    char categoria[TAM_TEXTO];
+    int encontrado = 0;
+
+    printf("\nDigite a categoria: ");
+    fgets(categoria, TAM_TEXTO, stdin);
+    categoria[strcspn(categoria, "\n")] = '\0';
+
+    for (int i = 0; i < total; i++) {
+        if (strstr(livros[i].categoria, categoria) != NULL) {
+            exibirLivro(livros[i]);
+            encontrado = 1;
+        }
+    }
+
+    if (!encontrado)
+        printf("\nNenhum livro encontrado.\n");
+}
+
+void buscarPorAno(Livro livros[], int total) {
+    int ano;
+    int encontrado = 0;
+
+    printf("\nDigite o ano: ");
+    scanf("%d", &ano);
+    limparBuffer();
+
+    for (int i = 0; i < total; i++) {
+        if (livros[i].ano == ano) {
+            exibirLivro(livros[i]);
+            encontrado = 1;
+        }
+    }
+
+    if (!encontrado)
+        printf("\nNenhum livro encontrado.\n");
 }
